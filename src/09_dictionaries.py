9d01334b5254111789ebc48adc28cf03dd21ebdf
abhitsahota <abhit.sahota@gmail.com>
2020-07-08 22:42:47 -0400

    completed 9 challenges

--- src/09_dictionaries.py
+++ src/09_dictionaries.py
@@ -35,6 +35,11 @@ waypoints = [
 
 # Add a new waypoint to the list
 # YOUR CODE HERE
+waypoints.append({
+    "lat": 99,
+    "lon": -89,
+    "name": "my house"
+})
 
 # Modify the dictionary with name "a place" such that its longitude
 # value is -130 and change its name to "not a real place"
@@ -42,6 +47,10 @@ waypoints = [
 # waypoints list.
 
 # YOUR CODE HERE
+waypoints[0].update({'lon':-130, 'name': 'not a real place' })
 
 # Write a loop that prints out all the field values for all the waypoints
 # YOUR CODE HERE
+for x in waypoints:
+    for y in x:
+        print(f'{y} : {x[y]}')
